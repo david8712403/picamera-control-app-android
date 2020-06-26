46c3faa888bcbec7b5c356a36cdd13ca9770722b
David <david87124@gmail.com>
2020-06-26 19:30:47 +0800

    mqtt branch init

---
+++ app/src/main/java/com/davidchen/unixfinalproject/MqttTest.java
@@ -0,0 +1,13 @@
+package com.davidchen.unixfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MqttTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mqtt_test);
+    }
+}
